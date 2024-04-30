93d0e19409e28114dd492d4e23d24a5e6915256e
Shravya Enugala <30644344+shravyae@users.noreply.github.com>
2024-04-30 14:47:30 -0600

    Update questions.sql

--- SQL Deep Dive/Like Operator/questions.sql
+++ SQL Deep Dive/Like Operator/questions.sql
@@ -5,15 +5,18 @@
 * Sample output: https://imgur.com/vXs4093
 * Use EXTRACT (YEAR FROM AGE(birth_date)) we will learn about this in later parts of the course
 */
-SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
-
-
+SELECT emp_no, first_name , EXTRACT (YEAR FROM AGE(birth_date)) AS age
+FROM employees
+Where first_name ILIKE 'M%';
 /*
 * DB: Employees
 * Table: employees
 * Question: How many people's name start with A and end with R?
 * Expected output: 1846
 */
+Select count(emp_no) 
+FROM employees
+where first_name like 'A%R';
 
                                                   
 /*
@@ -22,7 +25,9 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Question: How many people's zipcode have a 2 in it?.
 * Expected output: 4211 
 */
-
+SELECT count(customerid) 
+FROM customers
+WHERE zip::text LIKE '%2%';
 
 
 /*
@@ -31,7 +36,9 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Question: How many people's zipcode start with 2 with the 3rd character being a 1.
 * Expected output: 109 
 */
-
+SELECT count(customerid) 
+FROM customers
+WHERE zip::text LIKE '2_1%';
 
 /*
 * DB: Store
@@ -40,4 +47,7 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Replace null values with "No State"                                                  
 * Expected output: https://imgur.com/AVe6G4c
 */
+SELECT coalesce(state, 'No State') as "State" 
+FROM customers
+WHERE phone::text LIKE '302%';
 
